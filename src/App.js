1cc1608f9bf8f7fc7247ce894b5ccaed63b14877
lironbre <106016020+lironbre@users.noreply.github.com>
2022-11-28 23:44:01 +0200

    comments

--- src/App.js
+++ src/App.js
@@ -6,6 +6,25 @@ import { mainTheme } from './styles/mainTheme'
 import ConfirmationDialogProvider from './widgets/ConfirmationDialog/ConfirmationDialogProvider'
 import ShowDialogButtonContainer from './widgets/ShowDialogButton/containers/showDialogButtonContainer'
 
+/*
+verification - requirements ask!
+
+
+2 setters one after another is bad, race condition
+hardcoded - bad
+finally - in try catch 
+; - interptintion endof parsing -- https://flaviocopes.com/javascript-automatic-semicolon-insertion/
+unused imports - drop
+
+tree shaking - import in the code ?
+
+blue
+
+spertion of concept
+
+small. spearted file.
+ */
+
 function App() {
   return (
     <div className="App">
